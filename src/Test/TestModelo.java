f360d590ed98388ea29a97d96b97d7e523a63dec
dialmedu <diegomesa.1414@gmail.com>
2017-02-06 20:37:06 -0500

    Pruebas de implementacion

--- src/Test/TestModelo.java
+++ src/Test/TestModelo.java
@@ -13,14 +13,31 @@ import Modelo.*;
  */
 public class TestModelo {
 
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       
+        Televisor televisores[] = new Televisor[6];
+        Licuadora licuadoras[] = new Licuadora[6];
+        Labadora labadoras[] = new Labadora[6];
         
+        // vender 
+        televisores[0]= new Televisor();
+        televisores[1]= new Televisor();
+        televisores[2]= new Televisor();
+        televisores[3]= new Televisor();
+        televisores[4]= new Televisor();
+        televisores[5]= new Televisor();
         
+        System.out.println(televisores[1].getCantidad());
+        televisores[1].vender(1);
+        System.out.println(televisores[1].getCantidad());
+        System.out.println(televisores[1].existen());
+
+        televisores[1].vender(televisores[1].getCantidad());
+         
     }
     
 }
